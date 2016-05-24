ad8f3261a720c677608f2c82b7a4873bcf400e4b
Kevin Lyda <kevin@ie.suberic.net>
2016-05-24 17:27:14 +0100

    More work to get _LabelWrapper working.

--- src/_LabelWrapper.php
+++ src/_LabelWrapper.php
@@ -7,15 +7,24 @@ class _LabelWrapper
     public function __construct($metric_class, $var, $help, $labels)
     {
         $this->metric_class = $metric_class;
+        // TODO: Check var name is valid.
+        //       See: https://prometheus.io/docs/concepts/data_model/#metric-names-and-labels
         $this->var = $var;
         $this->help = $help;
+        // TODO: Check label names are valid.
+        //       See: https://prometheus.io/docs/concepts/data_model/#metric-names-and-labels
         $this->labels = $labels;
     }
 
     public function labels($label_values)
     {
-        $this->_metric_labels($label_values);
-        return new $this->metric_class($var, $help, $labels, $label_values);
+        if (count($this->labels) != count($label_values))
+        {
+            // TODO: Make exception classes.
+            throw new Exception("labels don't match.");
+        }
+        return new $this->metric_class($this->var, $this->help,
+                                       $this->labels, $label_values);
     }
 }
 
